0ca720c54287510539304ccfaf79de9cbe7bdf5d
Katherine Erickson <katherine.erickson@gmail.com>
2013-06-24 18:03:57 -0400

    migrate some content from old wordpress site

--- website/views.py
+++ website/views.py
@@ -1,7 +1,5 @@
-# Create your views here.
 from django.template import RequestContext # extends Context; needed for STATIC_URL
 from django.shortcuts import render_to_response, get_object_or_404 # r_to_r loads template, passes c    ontext, renders
-from django.db.models import Q # enables AND and OR in SQL filters
 from website.models import *
 
 def home(request):

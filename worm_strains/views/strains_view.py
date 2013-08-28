afa5d71501d2067fe0d61f35a3e8559279b1a6d8
katur <ke14@nyu.edu>
2013-08-28 14:35:38 -0400

    separate genotype generation into new function

--- worm_strains/views/strains_view.py
+++ worm_strains/views/strains_view.py
@@ -8,29 +8,19 @@ def strains(request):
 	"""
 	# get all worm strains
 	strains = WormStrain.objects.all().order_by('strain_sort')	
-	
-
-	"""
-	The following code, commented out, dynamically creates genotypes when a transgene and parent strain are listed.
-	For speed, I instead hard-coded these genotypes into the database
-	using a script that resembles the code below (scripts/insert_genotypes_into_database.py).
-	If the genotype format changes, it must be changed in the script and the script run to update the database.
-	If we want to change back to dynamically generated genotypes, uncomment the lines below.
-	"""
-	"""
 	for strain in strains:
-		if strain.transgene and strain.parent_strain:
-			vector = strain.transgene.vector
-			strain.genotype = strain.transgene.name + "[" + vector.parent_vector.genotype_pattern + "]"
-			strain.genotype = strain.genotype.replace("gene", vector.gene)
-			strain.genotype = strain.genotype.replace("promoter", vector.promoter)
-			strain.genotype = strain.genotype.replace("threePrimeUTR", vector.three_prime_utr)
-			if strain.parent_strain.name == "DP38":
-				strain.genotype = "unc-119(ed3) III; " + strain.genotype
-	"""
-	
-	
+		generate_genotype(strain)
 	# render page
 	return render_to_response('strains.html', {
 		'strains':strains
 	}, context_instance=RequestContext(request))
+
+def generate_genotype(strain):
+	if strain.transgene and strain.parent_strain:
+		vector = strain.transgene.vector
+		strain.genotype = strain.transgene.name + "[" + vector.parent_vector.genotype_pattern + "]"
+		strain.genotype = strain.genotype.replace("gene", vector.gene)
+		strain.genotype = strain.genotype.replace("promoter", vector.promoter)
+		strain.genotype = strain.genotype.replace("threePrimeUTR", vector.three_prime_utr)
+		if strain.parent_strain.name == "DP38":
+			strain.genotype = "unc-119(ed3) III; " + strain.genotype

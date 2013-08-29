2a300fdc6b80e568055edc60ec0a5284a0730011
katur <ke14@nyu.edu>
2013-08-29 17:04:05 -0400

    add container fields

--- storage/models.py
+++ storage/models.py
@@ -11,7 +11,20 @@ class Stock(models.Model):
 	date_prepared = models.DateField(null=True)
 	notes = models.TextField(blank=True)
 
+class ContainerSupertype(models.Model):
+	name = models.CharField(max_length=20)
+
+class ContainerType(models.Model):
+	name = models.CharField(max_length=50, blank=True)
+	supertype = models.ForeignKey(ContainerSupertype, null=True)
+	slots_vertical = models.IntegerField(null=True)
+	slots_horizontal = models.IntegerField(null=True)
+	image_filename = models.CharField(max_length=30, blank=True)
+	
 class Container(models.Model):
+	name = models.CharField(max_length=200, blank=True)
+	contents = models.CharField(max_length=200, blank=True)
+	type = models.ForeignKey(ContainerType, null=True)
 	vertical_position = models.PositiveSmallIntegerField(null=True)
 	horizontal_position = models.PositiveSmallIntegerField(null=True)
 	stock = models.ForeignKey(Stock, null=True)

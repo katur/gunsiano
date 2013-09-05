80b83263bc808ff89fe8b7783c3c580a40da141f
katur <ke14@nyu.edu>
2013-09-05 16:11:58 -0400

    add user login and logout pages. make worm portal show up only when logged in

--- gunsiano/settings.py
+++ gunsiano/settings.py
@@ -18,6 +18,8 @@ MANAGERS = ADMINS
 
 AUTH_PROFILE_MODULE = "website.UserProfile"
 
+LOGIN_REDIRECT_URL = "home_url"
+
 '''
 # include this, filled in, in settings_secret.py
 DATABASES = {
@@ -137,13 +139,13 @@ INSTALLED_APPS = (
 	'django.contrib.sites',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
-	
+
 	# Uncomment the next line to enable the admin:
 	'django.contrib.admin',
-	
+
 	# Uncomment the next line to enable admin documentation:
 	'django.contrib.admindocs',
-	
+
 	# Added by Katherine
 	'south', # for database migrations
 
--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+
 # urlpatterns is a module-level variable
 urlpatterns = patterns('website.views', # first arg prefix for views
 	# subsequent args are tuples: (regex, view, optional dictionary)
@@ -10,3 +11,9 @@ urlpatterns = patterns('website.views', # first arg prefix for views
 	url(r'^contact$', 'contact', name='contact_url'),
 	url(r'^worm_portal$', 'worm_portal', name='worm_portal_url'),
 )
+
+urlpatterns += url(r'^login/$', 'django.contrib.auth.views.login', {
+	'template_name':'login.html',
+}), url(r'^logout/$', 'django.contrib.auth.views.logout', {
+	'next_page':'/',
+}),

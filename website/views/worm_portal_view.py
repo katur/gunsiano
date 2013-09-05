b280b707a004c10b364408e2efe1734ac32cef54
katur <ke14@nyu.edu>
2013-09-05 17:31:35 -0400

    add style for worm portal. require login for worm portal view.

--- website/views/worm_portal_view.py
+++ website/views/worm_portal_view.py
@@ -1,7 +1,8 @@
-from django.template import RequestContext # extends Context; needed for STATIC_URL
-from django.shortcuts import render_to_response, get_object_or_404 # r_to_r loads template, passes c    ontext, renders
-from website.models import *
+from django.template import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def worm_portal(request):
 	"""
 	Worm Portal landing page

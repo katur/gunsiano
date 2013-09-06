c07aaf0aa6a68dc79f4c58380601e1f0c240b516
Katherine Erickson <katherine.erickson@gmail.com>
2013-09-05 22:16:55 -0400

    fix bug in footer positioning such that works with page resize

--- website/static/js/website.js
+++ website/static/js/website.js
@@ -1,16 +1,34 @@
 $(document).ready(function() {
+	setResizeHandler();
 	positionFooter();
 })
 
+setResizeHandler = function() {
+	$(window).resize(function() {
+		positionFooter();
+	})
+}
+
 positionFooter = function() {
+	footer = $("#wrap-footer");	
+	footerHeight = footer.outerHeight();
 	windowHeight = $(window).height();
 	contentHeight = $("html").height();
+
+	// if footer is absolute, it is not included in the content height, so add it
+	if (footer.css("position") == "absolute") {
+		contentHeight += footerHeight;
+	}
 	
-	if (contentHeight < windowHeight) {
-		$("#wrap-footer").css({
+	// if content is smaller than window, position the footer at bottom of page
+	// otherwise position it statically (necessary in case user resizes window)
+	footer.css(
+		(contentHeight < windowHeight) ? {
 			position: "absolute",
 			bottom: "0",
 			left: "0"
-		})
-	}
+		} : {
+			position: "static"
+		}
+	)
 }

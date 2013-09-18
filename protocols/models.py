88e9634c9eff77fe7bef4f3e4d2010b38b243a0c
katur <ke14@nyu.edu>
2013-09-18 13:33:32 -0400

    add unique constraints for protocol title and url

--- protocols/models.py
+++ protocols/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Protocol(models.Model):
-	title = models.CharField('Title', max_length=250)
-	url_title = models.CharField(max_length=250)
+	title = models.CharField('Title', max_length=250, unique=True)
+	url_title = models.CharField(max_length=250, unique=True)
 	author = models.ForeignKey(User, null=True, blank=True)
 	pub_date = models.DateField('Publication Date', auto_now=True)
 	body_markdown = models.TextField('Body', help_text='Use Markdown syntax.')

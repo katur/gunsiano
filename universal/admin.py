4c58dca5c6b479988980cf4d6fcfb42664f94358
katur <ke14@nyu.edu>
2013-09-18 15:18:28 -0400

    move user administration to universal. fix bug for non-superuser to edit their own User (UserAdmin.change_view relies on the password being in the fieldset)

---
+++ universal/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+class MyUserAdmin(UserAdmin):
+	list_display = UserAdmin.list_display + ('is_active', 'is_superuser',)
+
+	# for non-superusers, limit the editable users to the logged in user
+	def queryset(self, request):
+		qs = super(self.__class__, self).queryset(request)
+		if request.user.is_superuser:
+			return qs
+		return qs.filter(username=request.user)
+
+	# define a subset of safe fieldsets that non-superusers can access
+	safe_fieldsets = (
+		(None, {'fields': ('username', 'password')}),
+		(('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+	)
+
+	def change_view(self, request, object_id, extra_context=None):
+		if not request.user.is_superuser:
+			self.fieldsets = self.safe_fieldsets
+			return UserAdmin.change_view(self, request, object_id, extra_context=None)
+		else:
+			return UserAdmin.change_view(self, request, object_id, extra_context=None)
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
--- website/admin.py
+++ website/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from website.models import Position, UserProfile
-from django.contrib.auth.models import User
 
 class PositionAdmin(admin.ModelAdmin):
 	list_display = ('position', 'display_order')
-
 
 class UserProfileAdmin(admin.ModelAdmin):
 	list_display = (
@@ -29,32 +26,5 @@ class UserProfileAdmin(admin.ModelAdmin):
 			self.exclude = ('user', 'in_abu_dhabi', 'is_current', 'image_filename')
 		return super(UserProfileAdmin, self).get_form(request, obj, **kwargs)
 
-
-class MyUserAdmin(UserAdmin):
-	list_display = UserAdmin.list_display + ('is_active', 'is_superuser',)
-
-	# for non-superusers, limit the editable users to the logged in user
-	def queryset(self, request):
-		qs = super(self.__class__, self).queryset(request)
-		if request.user.is_superuser:
-			return qs
-		return qs.filter(username=request.user)
-
-	# define a subset of safe fieldsets that non-superusers can access
-	safe_fieldsets = (
-		(None, {'fields': ('username',)}),
-		(('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
-	)
-
-	def change_view(self, request, object_id, extra_context=None):
-		if not request.user.is_superuser:
-			self.fieldsets = self.safe_fieldsets
-			return UserAdmin.change_view(self, request, object_id, extra_context=None)
-		else:
-			return UserAdmin.change_view(self, request, object_id, extra_context=None)
-
-
 admin.site.register(Position, PositionAdmin)
-admin.site.unregister(User)
-admin.site.register(User, MyUserAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)

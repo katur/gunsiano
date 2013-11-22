bb38f11accd645e6a4434a633aeb27f16124e773
katur <ke14@nyu.edu>
2013-11-22 11:14:07 -0500

    deactivate moderation

--- website/moderator.py
+++ website/moderator.py
@@ -1,5 +1,5 @@
 from moderation import moderation
 from website.models import Resource, UserProfile
 
-moderation.register(Resource)
-moderation.register(UserProfile)
+# moderation.register(Resource)
+# moderation.register(UserProfile)

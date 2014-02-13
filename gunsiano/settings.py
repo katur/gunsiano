fbeba790c64277839d162388d0355567d3ee7a33
Katherine Erickson <katherine.erickson@gmail.com>
2014-02-13 12:12:43 -0500

    customizations to default django admin base template

--- gunsiano/settings.py
+++ gunsiano/settings.py
@@ -129,6 +129,7 @@ TEMPLATE_DIRS = (
 	# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
 	# Always use forward slashes, even on Windows.
 	# Don't forget to use absolute paths, not relative paths.
+	'/Users/katherine/ka73r/websites/gunsiano/universal/templates/'
 )
 
 INSTALLED_APPS = (

2634b3d8d182ab3da6fd3dfec28f0c83af5071de
Katherine Erickson <katherine.erickson@gmail.com>
2014-05-22 23:43:04 -0400

    move requirements back into one file now for simplicity. serve locally with gunicorn and py-static

--- gunsiano/settings.py
+++ gunsiano/settings.py
@@ -15,10 +15,6 @@ from local_settings import DEBUG, DATABASES, SECRET_KEY, LOCKDOWN_PASSWORDS
 #    }
 # }
 
-# To use dynamically generated roots throughout settings file.
-import os.path
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -30,68 +26,37 @@ MANAGERS = ADMINS
 LOGIN_URL = 'login_url'
 LOGIN_REDIRECT_URL = 'home_url'
 
-# Domain names that are valid for this site; required if DEBUG is False.
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 TIME_ZONE = 'America/New_York'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
-
 SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
 USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
 MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static'),
 )
 
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
@@ -110,7 +75,6 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'gunsiano.urls'
 
-# Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'gunsiano.wsgi.application'
 
 TEMPLATE_DIRS = (
@@ -126,7 +90,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
 
-    # Apps within the project
     'universal',
     'website',
     'worm_strains',
@@ -172,14 +135,13 @@ LOGGING = {
 # Everything below added by Katherine
 #####################################
 
-# To have request object in templates
+# For request object in templates
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 TEMPLATE_CONTEXT_PROCESSORS = TCP + (
     'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
 )
 
-# Constants
 MARKDOWN_PROMPT = (
     'Use Markdown syntax. See '
     '<a href="http://www.darkcoding.net/software/markdown-quick-reference">'
@@ -187,4 +149,3 @@ MARKDOWN_PROMPT = (
     '<a href="http://www.markdowntutorial.com/">a tutorial</a>, '
     'or practice <a href="http://dillinger.io/">here</a>.'
 )
-

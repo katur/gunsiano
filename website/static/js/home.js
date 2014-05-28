278d0c10e60f39107e07aba9caf5275fd803ff1b
Katherine Erickson <katherine.erickson@gmail.com>
2014-05-28 18:09:11 -0400

    debugging resize

--- website/static/js/home.js
+++ website/static/js/home.js
@@ -14,6 +14,7 @@ function expandResearchAreaHeight() {
   researchAreas.each(function() {
     var currentArea = $(this);
     if (currentArea.height() < minHeight) {
+      console.log(researchArea);
       console.log(currentArea.height());
       currentArea.height(minHeight);
       console.log(currentArea.height());

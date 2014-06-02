3e4c97d32161b62f1c26cffa59ce8f70c2123aaa
Katherine Erickson <katherine.erickson@gmail.com>
2014-06-02 17:56:09 -0400

    fix broken pubmed RSS feed (again). Fix Firefox style issues with hr's.

--- website/views.py
+++ website/views.py
@@ -106,9 +106,12 @@ def publications(request):
       (Gunsalus K[Author] OR Gunsalus KC[Author] NOT Gunsalus KT[Author])
     """
     xml_file = urllib2.urlopen('http://www.ncbi.nlm.nih.gov/entrez/eutils/'
-                               'erss.cgi?rss_guid=12Wu35auLMyC-bw6heubsB4Aa'
-                               '1vq6MH4xOLj1ILBD4Wimwikba')
+                               'erss.cgi?rss_guid=1F3_IGFYOIa3jmjM-4W8qYch'
+                               'jpJ613StRRzY4QE3ij-y6rvQ_H')
     # NOTE: below used to work; not sure why they stopped
+    # xml_file = urllib2.urlopen('http://www.ncbi.nlm.nih.gov/entrez/eutils/'
+    #                           'erss.cgi?rss_guid=12Wu35auLMyC-bw6heubsB4Aa'
+    #                           '1vq6MH4xOLj1ILBD4Wimwikba')
     # xml_file = urllib2.urlopen("http://www.ncbi.nlm.nih.gov/entrez/eutils/"
     #   "erss.cgi?rss_guid=18qVEVbjJjoq2mO-bQKE6E_-D4pje3l2O5Jd1cFE70SdwIYw_1")
     # xml_file = urllib2.urlopen("http://www.ncbi.nlm.nih.gov/entrez/eutils/"

39e4f5d0692df1c572f4e5ac17776c3613b9cc12
Katherine Erickson <katherine.erickson@gmail.com>
2014-06-11 18:47:45 -0400

    hide mouse-wrapper (not mouse itself)

--- website/static/js/home.js
+++ website/static/js/home.js
@@ -164,7 +164,7 @@ function startHomepageScrollEffects() {
     });
   } else {
     $("#evolution #mask").hide();
-    $("#mouse #drawing").hide();
+    $("#mouse #drawing-wrapper").hide();
     $("#network canvas").css("top", "-50%");
   }
 

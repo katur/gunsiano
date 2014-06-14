5fa378af24b76f288ade9bb6cc1d4a24e56f40f6
Katherine Erickson <katherine.erickson@gmail.com>
2014-06-13 21:26:17 -0400

    now fix bug where the fixed thead resize was not happening on window load

--- worm_strains/static/js/strains.js
+++ worm_strains/static/js/strains.js
@@ -5,19 +5,25 @@ $(document).ready(function() {
 })
 
 fixStrainsTopRow = function() {
+  var strainTable = $("#strain-table");
   var tableOffset = $("body#strains table").offset().top;
   var header = $("body#strains table thead").clone();
   var fixedHeader = $("body#strains #table-thead-fixed").append(header);
+  setFixedHeaderSize();
+
+  $(window).resize(setFixedHeaderSize);
 
   $(window).bind("scroll", function() {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance >= tableOffset && fixedHeader.is(":hidden")) {
-      var tableWidth = $("#strain-table").outerWidth();
-      fixedHeader.width(tableWidth);
-      console.log(tableWidth);
       fixedHeader.show();
     } else if ((scrollDistance < tableOffset) && fixedHeader.is(":visible")) {
       fixedHeader.hide();
     }
   });
+
+  function setFixedHeaderSize() {
+    var tableWidth = $("#strain-table").outerWidth();
+    fixedHeader.width(tableWidth);
+  }
 }

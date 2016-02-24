2e51abb214dc44407abf03a34e30fade46ded470
Katherine Erickson <katherine.erickson@gmail.com>
2016-02-24 17:45:02 -0500

    Remove optional field names

--- protocols/models.py
+++ protocols/models.py
@@ -5,14 +5,14 @@ from django.db import models
 
 
 class Protocol(models.Model):
-    title = models.CharField('Title', max_length=250, unique=True,
+    title = models.CharField(max_length=250, unique=True,
                              help_text=settings.MARKDOWN_PROMPT)
     title_url = models.CharField(max_length=250, unique=True)
     author = models.ForeignKey(
         User, models.SET_NULL, null=True, blank=True,
         help_text='If none selected, will default to logged in user.')
     pub_date = models.DateField('Publication Date', auto_now=True)
-    text = models.TextField('Body', help_text=settings.MARKDOWN_PROMPT)
+    text = models.TextField(help_text=settings.MARKDOWN_PROMPT)
 
     def __unicode__(self):
         return self.title

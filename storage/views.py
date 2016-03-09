bb8d1faed47667f2d70654b44e883e39a49bcf58
Katherine Erickson <katherine.erickson@gmail.com>
2016-03-09 13:48:08 -0500

    Always allow linking to container, even empty container

--- storage/views.py
+++ storage/views.py
@@ -23,41 +23,32 @@ def storage_detail(request, container_id):
     """
     container = get_object_or_404(Container, id=container_id)
 
-    unpositioned_children = container.container_set.filter(
-        is_thawed=0, horizontal_position__isnull=True,
-        vertical_position__isnull=True)
-
-    positioned_children = container.container_set.filter(
-        is_thawed=0, horizontal_position__isnull=False,
-        vertical_position__isnull=False)
+    children = container.container_set.filter(is_thawed=0)
 
     # If the child has a stock, attach the actual instance of the
     # stockable that makes up that stock (i.e. the specific
     # WormStrainLine, Primer, Antibody, etc.)
-    for child in unpositioned_children:
+    for child in children:
         if child.stock:
             child.stockable = child.stock.stockable.get_actual_instance()
 
-    for child in positioned_children:
-        if child.stock:
-            child.stockable = child.stock.stockable.get_actual_instance()
+    # Create 3D grid to hold positioned children.
+    # 3rd dimension is in case there are multiple objects within a
+    # slot (e.g., multiple plates within a freezer rack slot)
+    grid = [[list() for i in range(container.type.slots_horizontal)]
+            for j in range(container.type.slots_vertical)]
 
-    if not positioned_children:
-        grid = [[[container]]]
+    # Create list to hold unpositioned children.
+    unpositioned_children = []
 
-    else:
-        # Create an empty 3D grid.
-        # 3rd dimension is in case there are multiple objects within a
-        # slot (e.g., multiple plates within a freezer rack slot)
-        grid = [[list() for i in range(container.type.slots_horizontal)]
-                for j in range(container.type.slots_vertical)]
-
-        # Populate the grid with this container's children
-        for child in positioned_children:
+    for child in children:
+        if child.horizontal_position and child.vertical_position:
             x = child.horizontal_position - 1  # Positions are 1-indexed
             y = child.vertical_position - 1
-
             (grid[y][x]).append(child)
+
+        else:
+            unpositioned_children.append(child)
 
     context = {
         'container': container,
